f646abadb63694365d624640ad6f3e6d50ca2912
Claudia Gutierrez-Semprun <claudia@Claudias-MacBook-Air.local>
2024-05-16 09:22:27 +0300

    changes in the css and javaScript

--- index.js
+++ index.js
@@ -10,27 +10,40 @@ function getComputerChoice () {
   
   function getWinner (playerChoice, ComputerChoice) {
     if (playerChoice === ComputerChoice) {
-       alert("It's a tie");
+       return"It's a tie";
     } else if (
       (playerChoice === 'Img1.png' && ComputerChoice === 'Img3.png')||
        (playerChoice === 'Img2.png' && ComputerChoice === 'Img1.png')||
        (playerChoice === 'Img3.png' && ComputerChoice === 'Img2.png')
        ) {
-        alert("You win!");
+        return"You win!";
        } else {
-        alert("You Lose Fool!!");
+        return"You Lose Fool!!";
        }
   }
   
   
   document.getElementById('rock').addEventListener('click', ()=> game('Img1.png'));
   document.getElementById('paper').addEventListener('click', ()=> game('Img2.png'));
-  document.getElementById('sciccors').addEventListener('click', ()=> game('Img3.png'));
+  document.getElementById('scissors').addEventListener('click', ()=> game('Img3.png'));
   
   
   function game (playerChoice) {
     const ComputerChoice = getComputerChoice();
     const result = getWinner (playerChoice, ComputerChoice);
-    result = h1.body.innerHTML = "Good game!! ";
+  
+    var resultText = document.querySelector('h1');
+  
+    if (result === 'You win!') {
+      resultText.className = 'win';
+    } else if (result === 'You Lose Fool!!') {
+      resultText.className = 'lose';
+    } else {
+      resultText.className = 'tie';
+    }
+  
+    resultText.innerHTML = result;
   };
+
+  
   
